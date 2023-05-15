c850027d56c17619d3d6061860d16716272757cd
Stephen601 <stevewash256@gmai.com>
2023-05-15 08:22:34 -0600

    Login and logout functionality update

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///USH.sqlite'
 db = SQLAlchemy(app)
+app.secret_key = os.urandom(24)
+
 
 class Tools(db.Model):
-    __tablename__ = 'Tools'  # explicitly set the table name
+    __tablename__ = 'Tools'
     tool_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tool_name = db.Column(db.String(50), nullable=False)
     tool_price = db.Column(db.Float, nullable=False)
     tool_image = db.Column(db.String(50))
+
 
 class Users(db.Model):
     __tablename__ = 'Users'
@@ -19,14 +23,20 @@ class Users(db.Model):
     password = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50))
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if session.get('logged_in'):
+        return render_template('index.html', logged_in=True)
+    else:
+        return render_template('index.html', logged_in=False)
+
 
 @app.route('/tools')
 def tools():
     tools = Tools.query.distinct(Tools.tool_name).all()
     return render_template('tools.html', tools=tools)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -37,10 +47,22 @@ def login():
         user = Users.query.filter_by(username=username, password=password).first()
         if user is not None:
             # Successful login
+            session['logged_in'] = True
             return redirect(url_for('index'))
         else:
             error = 'Invalid credentials. Please try again.'
+    else:
+        # Check if the user is already logged in
+        if session.get('logged_in'):
+            return redirect(url_for('index'))
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
