ee524c3bab264fa8e0f354b50ff2bb27a8133b2e
Stephen601 <stevewash256@gmai.com>
2023-05-22 10:44:07 -0600

    Shopping update

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, json, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from datetime import datetime, time
+from flask_session import Session
+import random
+import string
+
+
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///USH.sqlite'
 db = SQLAlchemy(app)
 app.secret_key = os.urandom(24)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = '/tmp/flask_session'
+Session(app)
 
 
 class Tools(db.Model):
@@ -21,6 +31,7 @@ class Services(db.Model):
     service_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     service_description = db.Column(db.String(100), nullable=False)
     service_price = db.Column(db.Float, nullable=False)
+    service_image = db.Column(db.String(50))
 
 
 class Users(db.Model):
@@ -64,6 +75,7 @@ def login():
         if user is not None:
             # Successful login
             session['logged_in'] = True
+            session['user_key'] = ''.join(random.choices(string.ascii_letters + string.digits, k=24))
             return redirect(url_for('index'))
         else:
             error = 'Invalid credentials. Please try again.'
@@ -74,14 +86,49 @@ def login():
     return render_template('login.html', error=error)
 
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('index'))
 
-@app.route('/checkout')
+@app.route('/checkout', methods=['POST'])
 def checkout():
-    return render_template("checkout.html")
+    cart_data = request.form.get('cart')
+    # Process the cart data here
+
+    # For example, you can convert the JSON string back to a list of items
+    cart = json.loads(cart_data)
+
+    # You can then pass the cart data to the checkout page
+    return render_template('checkout.html', cart=cart)
+
+
+@app.route('/add-to-cart', methods=['POST'])
+def add_to_cart():
+    item_id = request.form.get('itemId')
+    item_name = request.form.get('itemName')
+    item_image = request.form.get('itemImage')
+    item_price = request.form.get('itemPrice')
+
+    cart = session.get('cart', [])
+    for item in cart:
+        if item['id'] == item_id:
+            item['quantity'] += 1
+            break
+    else:
+        cart.append({
+            'id': item_id,
+            'name': item_name,
+            'image': item_image,
+            'price': item_price,
+            'quantity': 1
+        })
+
+    session['cart'] = cart  # Store the updated cart data in the session
+
+    return 'Success'
+
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -111,6 +158,12 @@ def contact():
 def services():
     services = Services.query.all()
     return render_template('services.html', services=services)
+
+@app.route('/shop')
+def shop():
+    tools = Tools.query.distinct(Tools.tool_name).all()
+    services = Services.query.all()
+    return render_template('shop.html', tools=tools, services=services)
 
 
 
--- static/main.js
+++ static/main.js
@@ -1,13 +1,13 @@
 var cart = [];
 
-function addToCartTools(itemId, itemName, itemImage, itemPrice) {
+function addToCart(itemId, itemName, itemPrice) {
     // check if item already exists in cart
     for (var i = 0; i < cart.length; i++) {
         if (cart[i].id == itemId) {
-        // item already exists, update quantity
-        cart[i].quantity += 1;
-        updateCartTools();
-        return;
+            // item already exists, update quantity
+            cart[i].quantity += 1;
+            updateCart();
+            return;
         }
     }
 
@@ -15,27 +15,47 @@ function addToCartTools(itemId, itemName, itemImage, itemPrice) {
     cart.push({
         id: itemId,
         name: itemName,
-        image: itemImage,
-        price: itemPrice,
+        price: parseFloat(itemPrice).toFixed(2),
         quantity: 1
     });
+    
 
-    updateCartTools();
-    }
+    updateCart();
+}
 
-function updateCartTools() {
+function updateCart() {
     var total = 0;
     var cartHTML = '<ul>';
     for (var i = 0; i < cart.length; i++) {
         total += parseFloat(cart[i].price) * cart[i].quantity;
         cartHTML += '<li>';
-        if (cart[i].image) {
-        cartHTML += '<img src="/static/images/' + cart[i].image + '" alt="' + cart[i].name + '"> ';
-        }
         cartHTML += cart[i].name + ' - $' + cart[i].price + ' x ' + cart[i].quantity + '</li>';
     }
     cartHTML += '</ul>';
     cartHTML += '<p>Total: $' + total.toFixed(2) + '</p>';
 
     document.getElementById("cart").innerHTML = "Cart: " + cart.length + " items" + cartHTML;
-    }
+}
+
+function sendCartToCheckout() {
+    // Convert the cart data to JSON
+    var cartData = JSON.stringify(cart);
+
+    // Create a new form element
+    var form = document.createElement("form");
+    form.setAttribute("method", "POST");
+    form.setAttribute("action", "/checkout");
+
+    // Create a hidden input field to hold the cart data
+    var cartInput = document.createElement("input");
+    cartInput.setAttribute("type", "hidden");
+    cartInput.setAttribute("name", "cart");
+    cartInput.setAttribute("value", cartData);
+
+    // Add the input field to the form
+    form.appendChild(cartInput);
+
+    // Add the form to the document and submit it
+    document.body.appendChild(form);
+    form.submit();
+}
